2ca88da7307e8a40741dae0db1a4dcc65672ea29
yhm <1774072199@qq.com>
2021-07-10 08:37:17 +0800

    [flask001 flask响应常见场景]

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # flaskLearn
 Flask学习点滴
+
+# [fl001 Flask体验]
+1.常见的WEB框架
+    Django web  大而全，适合大型项目
+    Flask web   小而精，很丰富的第三方组件
+    tornado web 异步IO非阻塞，原生的websocket
+2、response的三种返回方式
+    1）HttpResponse
+    2) render_template
+    3) redirect
+    
+    jsonfy:返回json
+    send_file:上传文件
+
--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 # CreatDate: 2021/7/10 7:46
 # Description: Flask启动脚本
 
-from flask import Flask, make_response, redirect, jsonify, render_template
+from flask import Flask, make_response, redirect, jsonify, render_template, send_file
 import settings
 
 app = Flask(__name__)
@@ -15,8 +15,49 @@ app.config.from_object(settings.DEV)
 
 @app.route("/")
 def index():
+    """
+    直接响应 HttpResopnse
+    :return:
+    """
     return "<h1>Hello Flask</h1>"
 
 
+@app.route("/login")
+def login():
+    """
+    render_template 响应
+    :return:
+    """
+    return render_template("login.html")
+
+
+@app.route("/lg")
+def lg():
+    """
+    redirect重定向响应
+    :return:
+    """
+    return redirect("/login")
+
+
+@app.route("/json")
+def r_json():
+    """
+    返回json字符串  返回的是application/jsonfy
+    :return:
+    """
+    return jsonify({"name": "yhm", "age": 28})
+
+
+@app.route("/file")
+def res_json():
+    """
+    传输文件
+    :return:返回文件  向前端响应文件，可以是图片、视频、音频等等
+    """
+    return send_file("./static/street.jpg")
+
+
 if __name__ == "__main__":
+    # 启动服务
     app.run(host="0.0.0.0", port=8088)
